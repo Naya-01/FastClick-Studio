1627b6d848c3bc60d5e7b364717412473895242c
Naya-01 <abarkan.rayan@gmail.com>
2024-11-09 10:01:26 +0100

    add constant elements

--- src/data/pairs.js
+++ src/data/pairs.js
@@ -1,10 +1,17 @@
 import { Pair } from '../models/pair';
 
-export const lespairs = [
+export const affiche = [
   new Pair('FromDevice@1', 'Print@2'),
   new Pair('FromDevice@1', 'Host@6'),
   new Pair('FromDevice@1', 'Discard@4'),
   new Pair('Print@2', 'ToIPSummaryDump@3'),
   new Pair('ToIPSummaryDump@3', 'ToIPSummaryDumpLongLabel@3'),
   new Pair('ToIPSummaryDumpLongLabel@3', 'Discard@4'),
+];
+
+
+export const lespairs = [
+  new Pair('FromDevice@1', 'Print@2'),
+  new Pair('Print@2', 'Queue@3'),
+  new Pair('Queue@3', 'ToDevice@4'),
 ];
