a6398ce7ee075bcd515673b75c016ec3dbeef64d
Naya-01 <abarkan.rayan@gmail.com>
2025-04-03 18:11:58 +0200

    refactor: remove useless thing from index.html

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,9 @@ gulp.task('default', () => {
     .src('./build/index.html')
     .pipe(replace('.js"></script>', '.js" inline></script>'))
     .pipe(replace('rel="stylesheet">', 'rel="stylesheet" inline>'))
+    .pipe(replace(/<link rel="manifest" href=".*?"\/?>/g, ''))
+    .pipe(replace(/<link rel="apple-touch-icon" href=".*?"\/?>/g, ''))
+    .pipe(replace(/<link rel="icon" href=".*?"\/?>/g, ''))
     .pipe(
       inlinesource({
         compress: false,
