2bf809010dbbec3b74703d2c763b26723e6b6aca
Naya-01 <abarkan.rayan@gmail.com>
2025-04-10 13:44:58 +0200

    remove button '+'  when download images

--- src/hooks/useDownloadImage.js
+++ src/hooks/useDownloadImage.js
@@ -23,8 +23,18 @@ export function useDownloadImage(reactFlowWrapper) {
     const downloadFn = format === 'svg' ? toSvg : toPng;
     const extension = format === 'svg' ? 'svg' : 'png';
     
+    const noExportElements = reactFlowWrapper.current.querySelectorAll('.no-export');
+    const originalDisplay = [];
+    noExportElements.forEach((el, index) => {
+      originalDisplay[index] = el.style.display;
+      el.style.display = 'hidden'; 
+    });
+
     downloadFn(reactFlowWrapper.current, options)
       .then((dataUrl) => {
+        noExportElements.forEach((el, index) => {
+          el.style.display = originalDisplay[index];
+        });
         const link = document.createElement('a');
         link.download = `graph.${extension}`;
         link.href = dataUrl;
@@ -32,6 +42,9 @@ export function useDownloadImage(reactFlowWrapper) {
       })
       .catch((error) => {
         console.error("Could not download image", error);
+        noExportElements.forEach((el, index) => {
+          el.style.display = originalDisplay[index];
+        });
       });
   }, [reactFlowWrapper, getNodes]);
 }
